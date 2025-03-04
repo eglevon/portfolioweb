d5474c1705ff98e042e89f2697911509fe58cc44
Egle Vondrak <egle.vondrak@gmail.com>
2025-03-04 13:30:12 +0100

    Fixed projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,11 +2,11 @@ import Project from '../components/Project';
 import styles from './Projects.module.css';
 
 const projects = [
-    { title: 'WorldWide', link: 'https://vercel.com', img: '/src/assets/worldwide.png', description: 'WorldWide is an interactive and user-friendly application that helps travellers keep a log of places they have visited. It allows users to add new locations, view a list of all locations, and access details about each one.', tech: ['React', 'HTML', 'CSS Modules', 'JavaScript', 'Vite'], repo: 'https://github.com/' },
+    { title: 'WorldWide', link: 'https://vercel.com', img: '/src/assets/worldwide.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae id itaque ea hic eum obcaecati laboriosam eius accusamus dolorum culpa.', tech: ['React', 'HTML', 'CSS Modules', 'JavaScript', 'Vite'], repo: 'https://github.com/' },
 
-    { title: 'ReactQuiz', link: 'https://vercel.com', img: '/src/assets/reactquiz.png', description: 'A dynamic and interactive quiz application which allows users to test their knowledge with a series of questions while keeping track of their scores and high scores.', tech: ['React', 'HTML', 'CSS', 'TypeScript', 'Vite'], repo: 'https://github.com/' },
+    { title: 'React Quiz', link: 'https://vercel.com', img: '/src/assets/reactquiz.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae id itaque ea hic eum obcaecati laboriosam eius accusamus dolorum culpa.', tech: ['React', 'HTML', 'CSS', 'TypeScript', 'Vite'], repo: 'https://github.com/' },
 
-    { title: 'usePopcorns', link: 'https://vercel.com', img: '/src/assets/usepopcorns.png', description: 'usePopcorns is a web application for exploring movies, searching through an extensive database, and maintaining a personalized list of watched movies. The app includes features like rating movies, viewing detailed information, and dynamic search functionality.', tech: ['React', 'HTML', 'CSS Modules', 'TypeScript', 'Vite'], repo: 'https://github.com/' },
+    { title: 'usePopcorns', link: 'https://vercel.com', img: '/src/assets/usepopcorns.png', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae id itaque ea hic eum obcaecati laboriosam eius accusamus dolorum culpa.', tech: ['React', 'HTML', 'CSS Modules', 'TypeScript', 'Vite'], repo: 'https://github.com/' },
 ];
 
 function Projects() {
